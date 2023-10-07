6b237e9783f6dba222991970e804c98f1eb91390
FPG Schiba <CraftZockerLP@gmail.com>
2023-10-07 13:32:41 +0200

    New SW added

---
+++ zhaw/expd-1/Project.R
@@ -0,0 +1,3 @@
+csv_path <- paste(getwd(), '/data/BEV322OD3222.csv', sep='')
+dat <- read.csv(csv_path)
+
--- zhaw/prog-1/template.py
+++ zhaw/prog-1/template.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-PROG1 {V|P}{Nr:02d} page {page-nrs:02d}: Comments and prints
+PROG1 {V|P}{Nr:02d} page {page-nrs:02d|aufgabenNR}: Comments and prints
 
 @date: 28.09.2023
 @author: Jann Erhardt
