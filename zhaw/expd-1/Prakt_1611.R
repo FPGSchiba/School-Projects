d9a1862bee292c858a35be1606058520f06a8fba
FPG Schiba <CraftZockerLP@gmail.com>
2024-01-02 12:13:33 +0100

    Removed ZHAW

--- zhaw/expd-1/Prakt_1611.R
+++
@@ -1,2 +0,0 @@
-install.packages('tinytext')
-tinytex::install_tinytex()
--- zhaw/prog-1/sw6/testing.py
+++
@@ -1,4 +0,0 @@
-nums = [[1, 'one'], [2, 'two'], [3, 'three']]
-
-for a, num in enumerate(nums):
-    print(num[0] * num[1][-1] , end=' ')
